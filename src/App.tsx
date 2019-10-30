4796539520c8be0cc7c31b362f215a02a2bd2b00
Hyunsub Kim <embrapers263@gmail.com>
2019-10-30 10:22:48 +0000

    Added router and admin-layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import LoginPage from 'pages/login/login';
+import AdminLayout from 'pages/admin-layout';
 
 import 'antd/dist/antd.css';
 
 const App: React.FC = () => {
-  return <LoginPage />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route component={AdminLayout} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
