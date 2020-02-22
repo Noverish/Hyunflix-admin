25d87d908ef6203783bb95aaad015de735e7f992
Hyunsub Kim <embrapers263@gmail.com>
2020-02-22 06:46:50 +0000

    HTTPS!!

--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,12 @@ export const USER_INPUT_DEBOUNCE = 500;
 export const LONG_CLICK_TIME = 0.5;
 export const COLORS = ['magenta', 'red', 'volcano', 'orange', 'gold', 'lime', 'green', 'cyan', 'blue', 'geekblue', 'purple'];
 
-export const AUTH_SERVER = `http://${window.location.hostname}:8080/auth`;
-export const API_SERVER = `http://${window.location.hostname}:8080/api`;
-export const FS_SERVER = `http://${window.location.hostname}:8080/fs`;
+const { protocol, hostname } = window.location;
+export const AUTH_SERVER = `${protocol}//${hostname}:8080/auth`;
+export const API_SERVER = `${protocol}//${hostname}:8080/api`;
+export const FS_SERVER = `${protocol}//${hostname}:8080/fs`;
 
-export const SOCKET_SERVER = `http://${window.location.hostname}:8080`;
+export const SOCKET_SERVER = `${protocol}//${hostname}:8080`;
 export const FFMPEG_SOCKET_PATH = '/api/socket.io/ffmpeg';
 export const VIDEO_EXAMINE_SOCKET_PATH = '/api/socket.io/videos/examine';
 export const MUSIC_EXAMINE_SOCKET_PATH = '/api/socket.io/musics/examine';
